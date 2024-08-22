31b31c0d5e051940c340835e337257096d00dd15
oowarami <https://github.com/oowarami/yt2_html_basics.git>
2024-08-22 16:55:50 +0100

    created components and scscss folder

---
+++ yt2_js_library_react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Header />
+			<Hero />
+			{/* <About />
+			<Skills />
+			<Portfolio />
+			<Contact />
+			<Footer /> */}
+		</>
+	);
+}
+
+export default App;
